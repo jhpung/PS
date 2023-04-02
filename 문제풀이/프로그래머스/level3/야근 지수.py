e2b169339903a3c3f3c3dbbd117170780175e64b
jhpung <62597091+jhpung@users.noreply.github.com>
2023-04-02 22:14:45 +0900

    [programmers-lv3] 야근 지수

---
+++ 문제풀이/프로그래머스/level3/야근 지수.py
@@ -0,0 +1,31 @@
+from functools import reduce
+from heapq import heapify, heappush, heappop, heapreplace
+
+# 힙을 사용해 O(N)의 시간복잡도로 해결할 수 있다.
+
+
+def solution(n=int, works=list):
+    answer = 0
+
+    minimum = min(works)
+
+    heap = []
+
+    # heapify O(logN)
+    for i in works:
+        heappush(heap, (-i, i))
+
+    # O(N)
+    for i in range(n):
+        key, value = heappop(heap)
+
+        value = value - 1 if value - 1 >= 0 else 0
+        key = key + 1 if key + 1 <= 0 else 0
+
+        heappush(heap, (key, value))
+
+    # O(N)
+    for i in heap:
+        answer += i[1] ** 2
+
+    return answer
