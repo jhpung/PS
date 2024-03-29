df5969b3ec94871f0fad005917ab10ed31ad2741
jhpung <jhpung.dev@gmail.com>
2024-03-29 22:00:21 +0900

    Create 햄버거만들기.py

---
+++ 문제풀이/프로그래머스/level1/햄버거만들기.py
@@ -0,0 +1,22 @@
+B = 1
+V = 2
+M = 3
+
+BURGER = [B,V,M,B]
+
+def solution(ingredient = []):
+    answer = 0
+    
+    st1 = []
+    st2 = []
+    
+    for i in ingredient:
+        st1.append(i)
+        
+        if len(st1) < 4:
+            continue
+        
+        st1[0:-4]
+    
+    return answer
+                
