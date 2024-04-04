a2e72e4838cc7e085b441183e3fb10a46a5a967d
jhpung <jhpung.dev@gmail.com>
2024-04-04 14:38:59 +0900

    Update solution.py

--- 문제풀이/백준/히스토그램에서 가장 큰 직사각형/solution.py
+++ 문제풀이/백준/히스토그램에서 가장 큰 직사각형/solution.py
@@ -5,37 +5,39 @@ while True:
     
     n = inputs[0]
     
-    if len(inputs) == 1 and n == 0:
+    if n == 0:
         break
 
-    squares = inputs[1:] + [0]
+    squares = inputs[1:]
     stack = deque()
     answer = 0
     
     for index, height in enumerate(squares):
         if not stack:
             stack.append((index, height))
+            continue
         
-        while stack:
-            top_index, top_height = stack[-1]
-            
-            if top_height <= height:
-                break
-            
-            stack.pop()
-            
-            answer = max(((index - top_index) * top_height), answer)
+        top_index, top_height = stack[-1]
         
-        stack.append((index, height))
-    
+        if top_height < height:
+            stack.append((index, height))
+        else:
+            while stack:
+                top_index, top_height = stack[-1]
+            
+                if top_height < height:
+                    break
+            
+                stack.pop()
+            
+                answer = max(((index - top_index) * top_height), answer)
+                new_start = top_index
+            stack.append((new_start, height))
+
     while stack:
         top_index, top_height = stack.pop()
-        
-        if not stack:
-            width = n
-        else:
-            width = (n - top_index)
+        width = (n - top_index)
         answer = max(width * top_height, answer)
-    
+        
     print(answer)   
     
